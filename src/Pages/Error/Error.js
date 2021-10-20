f63c1a30df5dba53786fe3d0f67755788c9e0376
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2021-10-20 23:53:01 +0600

    edit somee file

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Footer from '../Home/Footer/Footer';
 import Header from '../Home/Header/Header';
+import error from '../../Images/404.webp';
 
 const Error = () => {
     return (
         <div>
             <Header></Header>
-            <h1>Error</h1>
+            <img src={error} className='ms-5 ps-5 w-50 d-flex justify-content-center ' alt="" />
             <Footer></Footer>
         </div>
     );
